0b409972833221fc60340a1befbe2dea89fe367c
isiagi <isiagigeofrey0@gmail.com>
2022-05-24 10:13:54 +0300

    add seoz

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,12 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <div>
+    <Head>
+        <meta charset="UTF-8" />
+        <meta name="keywords" content="hitex, consortium, hitexconsortium" />
+        <meta name="author" content="Paul" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
